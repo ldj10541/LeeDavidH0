2c682003659ae1714862e5baa797a7cda5498222
ldj10541 <ldj3219@naver.com>
2017-01-29 22:03:48 -0500

    Add files via upload

--- H0.java
+++ H0.java
@@ -2,7 +2,7 @@
 public class H0 {
 
 	public static void main(String[] args) {
-		//Prints out "Hello, World" on sccreen
+		//Prints out "Hello, World" on screen
 		System.out.println("Hello, World");
 	}
 
